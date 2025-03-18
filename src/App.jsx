e3d8bb8b1c672e33c7dd8e27750bd04482771fad
H-Anjum8 <anumanjum289@gmail.com>
2025-03-18 11:24:08 +0500

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+
+
+function App() {
+
+
+  return (
+    <>
+      <div className=" m-20">
+      <h1 className=" bg-red-200  ">Hello, Tailwind CSS!</h1>
+      <p className="bg-red-600   text-2xl">This is a Vite app with Tailwind CSS.</p>
+    </div>
+    </>
+  )
+}
+
+export default App
